d9d39eb026566024a9e20d446ccf6f04563b85b1
fyras23 <115085893+fyras23@users.noreply.github.com>
2023-04-28 02:01:32 +0100

    Add files via upload

---
+++ main/java/com/firas/weapon1/Weapon1Application.java
@@ -0,0 +1,13 @@
+package com.firas.weapon1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weapon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Weapon1Application.class, args);
+	}
+
+}
